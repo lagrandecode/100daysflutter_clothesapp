6fc2d4d097a27edb521ec791db782e10697637a1
lagrandecode <seunogunmolufirst1@gmail.com>
2024-01-01 18:24:25 +0100

    commit

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SearchBar extends StatelessWidget {
   const SearchBar({Key? key}) : super(key: key);
@@ -6,7 +7,35 @@ class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      child: Row(
+        children: [
+          Flexible(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                cursorColor: Colors.grey.withOpacity(0.3),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search_off_outlined),
+                  hintText: "Search For the Nice Clothes",
+                  hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                  fillColor: Colors.red,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            flex: 1,
+          ),
+          Container(
+            width: 50,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10)
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
